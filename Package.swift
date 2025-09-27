8c21c36f68d5d0f745d3219fd2d0e8d71c567460
SuperGamer474 <223823408+SuperGamer474@users.noreply.github.com>
2025-09-27 15:41:59 +0930

    Add dependencies for ZIPFoundation and ZsignPackage

--- Package.swift
+++ Package.swift
@@ -12,9 +12,17 @@ let package = Package(
             targets: ["ProStoreTools"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", branch: "main"),
+        .package(url: "https://github.com/khcrysalis/Zsign-Package.git", branch: "package")
+    ],
     targets: [
         .target(
             name: "ProStoreTools",
+            dependencies: [
+                .product(name: "ZIPFoundation", package: "ZIPFoundation"),
+                .product(name: "ZsignSwift", package: "ZsignPackage"),
+            ],
             path: "Sources/ProStoreTools"
         ),
     ]
